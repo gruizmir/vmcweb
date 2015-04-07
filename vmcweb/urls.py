e665c05396cb7621f77afea0c2a21ae90f80fc74
Gabriel Ruiz M <gabriel.ruiz.miranda@gmail.com>
2015-04-07 17:16:44 -0300

    Resp

--- vmcweb/urls.py
+++ vmcweb/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'main.views.home', name='home'),
+    url(r'^register/hackathon/', 'main.views.register_hack_team',
+                                    name='hack_register'),
     url(r'^admin/', include(admin.site.urls)),
 )
