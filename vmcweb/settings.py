be6ac3d4e59b8ceb483606c770cc2f939887f43c
Gabriel Ruiz <gabriel@destacame.cl>
2016-05-15 07:54:20 -0400

    Cambia ubicación de archivos estáticos y agrega galería de fotos

--- vmcweb/settings.py
+++ vmcweb/settings.py
@@ -100,7 +100,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'vmcweb/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATE_DIRS = (
