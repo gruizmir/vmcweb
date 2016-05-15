de9ede02f421a1e1408e7887ac894b4f462e5f48
Gabriel Ruiz <gabriel@destacame.cl>
2016-05-15 02:42:10 -0400

    Fuerza los archivos estáticos

---
+++ main/static/2015/js/form-validation.js
@@ -0,0 +1,14 @@
+// Form validation - register form
+    toastr.options = {"positionClass": "toast-top-full-width"};
+    $('.form-register').validate({
+      messages: {
+        name: "Debes ingresar tu nombre",
+        lastname: "Debes ingresar tu apellido",
+        email: "Debes ingresar un email válido",
+        app_name: "Debes ingresar un nombre válido",
+        description: "Debes ingresar una descripción válida",
+        leader: "Debes registrar un líder de equipo",
+        person2: "Mínimo 3 integrantes",
+        person3: "Mínimo 3 integrantes",
+      },
+    });
